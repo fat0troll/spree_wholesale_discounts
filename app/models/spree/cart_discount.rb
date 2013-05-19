1fcc5fb17edb0e5bb834beb86acfed026dc97bb9
fat0troll <fat0troll@yandex.ru>
2013-05-19 23:57:56 +0400

    attibutes for cart discount

--- app/models/spree/cart_discount.rb
+++ app/models/spree/cart_discount.rb
@@ -1,6 +1,7 @@
 class Spree::CartDiscount < ActiveRecord::Base
   belongs_to :variant, :touch => true
   acts_as_list :scope => :variant
+  attr_accessible :variant, :minimal_price, :amount
 
   validates :variant, presence: true # link to variant, that will be used
   validates :minimal_price, presence: true # minimal price. If we have variant with price, which beats this, we use that variant.
