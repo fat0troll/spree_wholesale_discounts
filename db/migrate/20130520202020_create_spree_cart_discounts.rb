ff1daa9fd3c6a4083c96f30c0cbeb5549a82f721
fat0troll <fat0troll@yandex.ru>
2013-05-19 23:25:50 +0400

    fix typo in migration

--- db/migrate/20130520202020_create_spree_cart_discounts.rb
+++ db/migrate/20130520202020_create_spree_cart_discounts.rb
@@ -1,6 +1,6 @@
 class CreateSpreeCartDiscounts < ActiveRecord::Migration
   def self.up
-    create_table :spree_cart_discountss do |t|
+    create_table :spree_cart_discounts do |t|
       t.decimal :amount, :precision => 8, :scale => 2
       t.decimal :minimal_price, :precision => 8, :scale => 2
       t.references :variant
@@ -9,6 +9,6 @@ class CreateSpreeCartDiscounts < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table :spree_group_prices
+    drop_table :spree_cart_discounts
   end
 end
