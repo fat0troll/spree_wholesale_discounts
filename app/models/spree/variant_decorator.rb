d76af79f390584320ab6f76e06434dd86ee5b659
fat0troll <fat0troll@yandex.ru>
2013-05-20 19:49:19 +0400

    experiments [2]

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -6,16 +6,16 @@ Spree::Variant.class_eval do
 
   # Calculating the price based on cart's total amount
   def discount(amount)
-    if self.cart_discounts.count == 0
+  #  if self.cart_discounts.count == 0
+  #    return self.price
+  #  else
+  #    self.cart_discounts.each do |disco|
+  #      if disco.minimal_price < amount
+  #        return disco.amount
+  #      end
+  #    end
+  #    # No matching discount, buy MOAR, please :)
       return self.price
-    else
-      self.cart_discounts.each do |disco|
-        if disco.minimal_price < amount
-          return disco.amount
-        end
-      end
-      # No matching discount, buy MOAR, please :)
-      return self.price
-    end
+  #  end
   end
 end
