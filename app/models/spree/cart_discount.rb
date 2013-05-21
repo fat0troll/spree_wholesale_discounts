31fb8a1a7a234fd7892aa1403e568d7fe5263108
fat0troll <fat0troll@yandex.ru>
2013-05-21 23:15:18 +0400

    experiments [8]

--- app/models/spree/cart_discount.rb
+++ app/models/spree/cart_discount.rb
@@ -2,9 +2,9 @@ class Spree::CartDiscount < ActiveRecord::Base
   belongs_to :product, :touch => true
   belongs_to :level
   acts_as_list :scope => :product
-  attr_accessible :product, :level_id, :amount
+  attr_accessible :product_id, :level_id, :amount
 
-  validates :product, presence: true # link to variant, that will be used
+  validates :product_id, presence: true # link to variant, that will be used
   validates :level_id, presence: true
   validates :amount, presence: true # price, fucking price...
 end
